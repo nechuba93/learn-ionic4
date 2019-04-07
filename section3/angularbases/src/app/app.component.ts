87df6fbcbd951f60d5fceb4165068999f2dd5564
Nacho <iortega@atsistemas.com>
2019-04-07 20:34:11 +0200

    add bootstrap

--- section3/angularbases/src/app/app.component.ts
+++ section3/angularbases/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  styles: [
+    '@import "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";'
+  ]
 })
 export class AppComponent {
   title = 'angularbases';
